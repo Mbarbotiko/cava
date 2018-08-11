f3a0c9b0b4ba334597e64c87f6c2d8099d04adc6
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-08-11 17:56:40 -0500

    adding jquery, switching to another branch for css styling and jquery

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+// var dishOne=$('#black');
+// $(dishOne).hide();
  
+// $('.dish1').hover(
+//     function () {
+//       $(dishOne).show();
+//       $(".dish1").fadeTo(.5)
+      
+//     }, 
+//     function () {
+//       $(dishOne).hide()
+//       $(".dish1").css("background-color", "none", "background-image", "url(../images/dish1.jpg");
+      
 
-});
+//       }
+    
+//   );
 
+
+// });
