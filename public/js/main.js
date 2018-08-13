10e23b1171dbe462f4fff1f5d3ee418ed792c090
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-08-12 21:12:16 -0500

    adding some media screen sizes, need to complete mobile responsiveness and onclick function for recipes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  
+
 //Onclick nav "Location" & "Reviews" using JQuery animate method to set hash in html to scroll to the set window location in 2 seconds.
   $(".loc").on('click',function(){
     if (this.hash !== "") {
