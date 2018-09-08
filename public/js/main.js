9c08cec130547d07c52064749dbd944106f4cd15
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-09-08 12:32:35 -0500

    changed length of reviews, set time out length, positioning of reviews section

--- public/js/main.js
+++ public/js/main.js
@@ -37,28 +37,28 @@ function Review(header, comment){
 
 const firstReview= new Review ("<h1>Woooow!</h1>","<p>This is the second time in this restaurant and I am really impressed by the quality of food, professionalism and the level of service</p>")
 
-const secondReview= new Review ("<h1>Really Really Really Great Food!</h1>","<p>First class food, first class service and a great ambiance. This was my favorite restaurant in Copenhagen. Food is served in small mini portions giving you chance to sample, should you wish, two types of starter and two of the main course and even two deserts should you be hungry. The quality and flavours were of the dishes were exquisite and everything was presented beautifully. Well done Quote. Im a tough customer to impress</p>")
+const secondReview= new Review ("<h1>Really Really Really Great Food!</h1>","<p>First class food, first class service and a great ambiance. The quality and flavours were of the dishes were exquisite and everything was presented beautifully. Well done Quote. Im a tough customer to impress</p>")
 
-const thirdReview= new Review ("<h1>Lovely atmosphere, stylish but not snobbish</h1>","<p>We had a lovely meal at this place. The food was terrific, the location was central and the service was excellent. I don't like snobbish places so this was the perfect combo of nice atmosphere and good food. We went for the recommendations and were not disappointed.</p>")
+const thirdReview= new Review ("<h1>Lovely atmosphere, stylish but not snobbish</h1>","<p>We had a lovely meal at this place. The food was terrific, the location was central and the service was excellent.  We went for the recommendations and were not disappointed.</p>")
 
 
 
 showReviews();
 
 function showReviews(){
-  $('#show-reviews').html(firstReview.header+firstReview.comment).fadeIn(2000).fadeOut(2000);
-  setTimeout(showReviews2, 4000);
+  $('#show-reviews').html(firstReview.header+firstReview.comment).fadeIn(2000).fadeOut(6000);
+  setTimeout(showReviews2, 8000);
 
 }
 
 function showReviews2(){
-  $('#show-reviews').html(secondReview.header+secondReview.comment).fadeIn(2000).fadeOut(2000);
-  setTimeout(showReviews3, 4000);
+  $('#show-reviews').html(secondReview.header+secondReview.comment).fadeIn(2000).fadeOut(6000);
+  setTimeout(showReviews3, 8000);
 
 }
 function showReviews3(){
-  $('#show-reviews').html(thirdReview.header+thirdReview.comment).fadeIn(2000).fadeOut(2000);
-  setTimeout(showReviews, 4000);
+  $('#show-reviews').html(thirdReview.header+thirdReview.comment).fadeIn(2000).fadeOut(6000);
+  setTimeout(showReviews, 8000);
 
 }
 
