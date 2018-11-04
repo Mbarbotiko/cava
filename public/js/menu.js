5b613fe912a610cef94c7265d7dfc54b79675a82
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-11-04 10:55:28 -0600

    adding style to headers font change organized table, ready for ajax calls

---
+++ public/js/menu.js
@@ -0,0 +1,5 @@
+
+let headerWidth = document.getElementById("main-header").offsetWidth
+console.log(headerWidth)
+//can do this. css to kee the menu centered on screen change add event listener and logic divide width  by screen width @ time multiply by 100 
+//124 px/ 1366 px= .9 * 100 = 90, 90/2 = 45 (center of screen)
