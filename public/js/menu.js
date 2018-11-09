7850dce413ac70d70350245020560ae55502ef59
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-11-09 12:25:23 -0600

    fixing ajax call, less code, more clean, can adjust with CSS for media screens

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,62 +1,29 @@
 $(document).ready(function () {
-
-    let headerWidth = document.getElementById("main-header").offsetWidth
-    console.log(headerWidth)
-    //can do this. css to kee the menu centered on screen change add event listener and logic divide width  by screen width @ time multiply by 100 
-    //124 px/ 1366 px= .9 * 100 = 90, 90/2 = 45 (center of screen)
     
 
-
+// add error handling for ajax call and loading icon  because heroku is slow to load
     $.ajax({
         url: 'https://cava-manager.herokuapp.com/api/dishes',
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        let appCount = 0
-        let entreeCount = 0
-        let drinksCount = 0
         let tableType = null;
         response.map(function (cavaRes) {
             if (cavaRes.type === "Appetizer") {
-                tableType = '.appetizer-table'
-                appCount++
-                console.log(appCount)
-
-                if (appCount % 2 === 0) {
-                    $(tableType).append('<th>' + cavaRes.title + '<p>' + cavaRes.description + '</p>' + '</th>')
-                    $(tableType).append('</tr>')
-                } else {
-                    $(tableType).append('<tr>')
-                    $(tableType).append('<th>' + cavaRes.title + '<p>' + cavaRes.description + '</p>' + '</th>')
-                }
+                tableType = '.menu-appetizers'
+                $(tableType).append('<table><tr><th>'+cavaRes.title+'<p>'+cavaRes.description+'</p></th></tr></table')
+                
             }
             else if (cavaRes.type === "Entree") {
-                tableType = '.entree-table'
-                entreeCount++
-                console.log(appCount)
-
-                if (entreeCount % 2 === 0) {
-                    $(tableType).append('<th>' + cavaRes.title + '<p>' + cavaRes.description + '</p>' + '</th>')
-                    $(tableType).append('</tr>')
-                } else {
-                    $(tableType).append('<tr>')
-                    $(tableType).append('<th>' + cavaRes.title + '<p>' + cavaRes.description + '</p>' + '</th>')
-                }
+                tableType = '.menu-entree'
+                $(tableType).append('<table><tr><th>'+cavaRes.title+'<p>'+cavaRes.description+'</p></th></tr></table')
+               
             }
 
             else if (cavaRes.type === "Drinks") {
-                tableType = '.drinks-table'
-                drinksCount++
-                console.log(appCount)
-
-                
-                if (drinksCount % 2 === 0) {
-                    $(tableType).append('<th>' + cavaRes.title + '<p>' + cavaRes.description + '</p>' + '</th>')
-                    $(tableType).append('</tr>')
-                } else {
-                    $(tableType).append('<tr>')
-                    $(tableType).append('<th>' + cavaRes.title + '<p>' + cavaRes.description + '</p>' + '</th>')
-                }
+                tableType = '.menu-drinks'
+                $(tableType).append('<table><tr><th>'+cavaRes.title+'<p>'+cavaRes.description+'</p></th></tr></table')
+             
 
 
 
@@ -64,13 +31,13 @@ $(document).ready(function () {
                 console.log('No Menu Available contact Admin')
             }
         })
-    });
-    //use map
-    //if type.response ==="Appetizer" print to .appetizer-table
-    //"Entree"  /entree-table
-    //"Drinks"/drinks-table
-    //create <th>response.title</th>
-    //<td>reponse.description</td>
+   });
+    // use map
+    // if type.response ==="Appetizer" print to .appetizer-table
+    // "Entree"  /entree-table
+    // "Drinks"/drinks-table
+    // create <th>response.title</th>
+    // <td>reponse.description</td>
 
 
 });
