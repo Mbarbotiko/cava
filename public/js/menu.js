c0b315fefe878303fd891a40a410c21da0ccf261
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-11-09 13:07:17 -0600

    figuring out length of strings to add .... to menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
-    
+    // let headerWidth = document.getElementById("main-header").offsetWidth
+    // console.log(headerWidth)
+
+
+    // let pixelsAcross = document.getElementById('howManyPixelsAcross').offsetWidth;
+    // console.log(pixelsAcross); //677 pixels
+
+    // let charactersAcross = document.getElementById('howManyPixelsAcross').innerHTML;
+
+    // let across = charactersAcross.length//45 M's across
+
+    // console.log(across);
 
 // add error handling for ajax call and loading icon  because heroku is slow to load
     $.ajax({
         url: 'https://cava-manager.herokuapp.com/api/dishes',
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+     
         let tableType = null;
         response.map(function (cavaRes) {
             if (cavaRes.type === "Appetizer") {
@@ -17,6 +28,7 @@ $(document).ready(function () {
             else if (cavaRes.type === "Entree") {
                 tableType = '.menu-entree'
                 $(tableType).append('<table><tr><th>'+cavaRes.title+'<p>'+cavaRes.description+'</p></th></tr></table')
+      
                
             }
 
