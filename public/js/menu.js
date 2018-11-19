aa4ddbffe152d468591a14c55297d476e91436ff
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-11-19 08:02:27 -0600

    adding loading image

--- public/js/menu.js
+++ public/js/menu.js
@@ -10,63 +10,54 @@ $(document).ready(function () {
 //use that to count the string added to the description and check its last . coordinate/ position
 
 
-let testTable = document.getElementById('test');
-console.log(testTable)
-let leftPoint = testTable.getBoundingClientRect();
-console.log(leftPoint);//use the width of element- width of string element
-let stringEl = document.getElementById('ptest').innerHTML
-console.log(stringEl);
-let stringLength = stringEl.length//51 characters long.
-let stringIndex = stringLength-1;
-let lastLetter = stringEl[stringIndex]
-console.log(lastLetter)//returns the last letter in the string
+// let testTable = document.getElementById('test');
+// console.log(testTable)
+// let leftPoint = testTable.getBoundingClientRect();
+// console.log(leftPoint);//use the width of element- width of string element
+// let stringEl = document.getElementById('ptest').innerHTML
+// console.log(stringEl);
+// let stringLength = stringEl.length//51 characters long.
+// let stringIndex = stringLength-1;
+// let lastLetter = stringEl[stringIndex]
+// console.log(lastLetter)//returns the last letter in the string
 
 //find the x position of the last Letter vs the x position of the edge of the table
 
 // let width = window.innerWidth;
 // console.log(width);
 
-let tryThis = lastLetter.getBoundingClientRect();
-console.log(tryThis);
+// let tryThis = lastLetter.getBoundingClientRect();
+// console.log(tryThis);
+let loading = '<div id ="loader"><img src="./public/images/loader.gif"></div>'
+$('.bg-menu-header').prepend(loading);
+let loadingDiv = '#loader'
 
-
+$
     $.ajax({
         url: 'https://cava-manager.herokuapp.com/api/dishes',
         method: "GET"
+
     }).then(function (response) {
+        $(loadingDiv).hide();
      
         let tableType = null;
         response.map(function (cavaRes) {
             if (cavaRes.type === "Appetizer") {
                 tableType = '.menu-appetizers'
-                $(tableType).append('<table><tr><th>'+cavaRes.title+'<p>'+cavaRes.description+'</p></th></tr></table')
-                
+                $(tableType).append('<table><tr><th>'+cavaRes.title+'<p>'+cavaRes.description+'</p></th></tr></table')   
             }
             else if (cavaRes.type === "Entree") {
                 tableType = '.menu-entree'
                 $(tableType).append('<table><tr><th>'+cavaRes.title+'<p>'+cavaRes.description+'</p></th></tr></table')
-      
-               
             }
 
             else if (cavaRes.type === "Drinks") {
                 tableType = '.menu-drinks'
                 $(tableType).append('<table><tr><th>'+cavaRes.title+'<p>'+cavaRes.description+'</p></th></tr></table')
-             
-
-
-
             } else {
-                console.log('No Menu Available contact Admin')
+
+                alert('No Menu Available contact Admin')
             }
         })
    });
-    // use map
-    // if type.response ==="Appetizer" print to .appetizer-table
-    // "Entree"  /entree-table
-    // "Drinks"/drinks-table
-    // create <th>response.title</th>
-    // <td>reponse.description</td>
-
-
 });
