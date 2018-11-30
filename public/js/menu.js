6c57662c1d19560de368f75111630f499ed068e9
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-11-30 08:07:25 -0600

    adding comments for next steps

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,44 +3,10 @@ $(document).ready(function () {
 
     // add error handling for ajax call and loading icon  because heroku is slow to load
 
-    //get the position of the table
-    //get the position of the last . in the newly created string from lop
-    //loop until that position matches the position of the right edge of the table
-    //create variable that will change with each map loop through the object ++ so a unique ID is created for the element
-    //use that to count the string added to the description and check its last . coordinate/ position
-
-
-    // let testTable = document.getElementById('test');
-    // console.log(testTable)
-    // let leftPoint = testTable.getBoundingClientRect();
-    // console.log(leftPoint);//use the width of element- width of string element
-    // let stringEl = document.getElementById('ptest').innerHTML
-    // console.log(stringEl);
-    // let stringLength = stringEl.length//51 characters long.
-    // let stringIndex = stringLength-1;
-    // let lastLetter = stringEl[stringIndex]
-    // console.log(lastLetter)//returns the last letter in the string
-
-    //find the x position of the last Letter vs the x position of the edge of the table
-
-    // let width = window.innerWidth;
-    // console.log(width);
-
-    // let tryThis = lastLetter.getBoundingClientRect();
-    // console.log(tryThis);
-
-    // let testTable = document.getElementById('test');
-    // console.log(testTable);
-    // let stringEl = document.getElementById('ptest').innerText;
-    // console.log(stringEl);
-    // let stringIndex = stringEl.length-1
-    // console.log(stringIndex)
-    // let lastCharacter = stringEl[stringIndex]
-    // console.log(lastCharacter)
-
-
     let testTable = document.getElementById('test');
-    console.log(testTable);
+    console.log(testTable)
+    let leftPoint = testTable.getBoundingClientRect();
+    console.log(leftPoint);//use the width of element- width of string element
     let stringEl = document.getElementById('ptest').innerText;
     console.log(stringEl);
     let lastCharacter = stringEl.lastIndexOf('.')
@@ -54,7 +20,11 @@ $(document).ready(function () {
     console.log(whereIsIt);
     console.log(stringEl)
 
+//create a loop that appends text select the last character of the string and append it using a span tag then use the ID of the span tag to check its position in the window.
 
+//if it has not reached the outer most edge of the table coordinate remove the span tag and continue the loop
+
+//if it has end the function
 
     //https://stackoverflow.com/questions/5143534/how-to-get-the-position-of-text-within-an-element
 
