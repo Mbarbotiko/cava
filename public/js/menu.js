a9f60aff9f4ddf22bce0b915dcf99cb4ad5a7433
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-12-01 13:05:56 -0600

    adding function for dots with minor issues to fix later

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,14 +1,7 @@
 $(document).ready(function () {
-
-
     let loading = '<div class="loader"><img src="./public/images/loader.gif"></div>'
-
-    $(loading).css({ "color": "purple" })
-
     $('.menu-header').prepend(loading);
     let loadingDiv = '.loader'
-
-
     $.ajax({
         url: 'https://cava-manager.herokuapp.com/api/dishes',
         method: "GET"
@@ -45,78 +38,32 @@ $(document).ready(function () {
     // let leftPoint = testTable.getBoundingClientRect();
     // console.log(leftPoint);
 
-    // function addToString() {
-    //     let stringEl = document.getElementById('ptest').innerText;
-    //     console.log(stringEl);
-    //     $('#ptest').append('<span class = "dots">  .</span>')
-    //     let findMe = $('.dots').position()
-    //     console.log(findMe)
-    //     let outerPoint = Math.round(findMe.left)
-    //     console.log(outerPoint)
-    //     // $('.dots').remove()
+    //prints dots to the p tags within the table element, will want to replace 100 with a length or do a while loop instead
+    //outer point will need to be replaced at aswell, last p tag dots run past because using from LEFT instead of from right elements write from left to right every other P tag should cross check against left point, even p tag should check against right point
+    function dots(x) {
+        for (var i = 0; i <100; i++) {
+            $(x).append('  .' + '<span class = "dots">  .</span>')
+            findMe = $('.dots').position()
+            outerPoint = Math.round(findMe.left)
+            console.log(outerPoint)
+            $('.dots').remove()
+            if (outerPoint > 675) {
+                $('.dots').remove()
+                break;
+            }
 
-    //     for (var i = 0; i < 25; i++) {
-    //         $('.dots').remove()
-    //         $('#ptest').append('  .' + '<span class = "dots">  .</span>')
-    //         findMe = $('.dots').position()
-    //         console.log(findMe)
-    //         outerPoint = Math.round(findMe.left)
-    //         console.log(outerPoint)
-    //         if (outerPoint>600){
-    //             $('.dots').remove()
-    //             break;
-    //         }
-
-    //     }
-
-    // }
-
-
-    function addToString() {
-        let table = document.getElementsByTagName('table')
-        for(var i = 0; i<table.length; i++){
-            let p = table[i].getElementsByTagName('p')[0]
-            console.log(p)
         }
-
-        // let stringEl = document.getElementsByTagName('p')
-        // for (var i = 0 ; i<stringEl.length; i++) {
-        //     console.log(stringEl[i])
-
-        // }
-        // $('#ptest').append('<span class = "dots">  .</span>')
-        // let findMe = $('.dots').position()
-        // console.log(findMe)
-        // let outerPoint = Math.round(findMe.left)
-        // console.log(outerPoint)
-        // // $('.dots').remove()
-
-        // for (var i = 0; i < 25; i++) {
-        //     $('.dots').remove()
-        //     $('#ptest').append('  .' + '<span class = "dots">  .</span>')
-        //     findMe = $('.dots').position()
-        //     console.log(findMe)
-        //     outerPoint = Math.round(findMe.left)
-        //     console.log(outerPoint)
-        //     if (outerPoint>600){
-        //         $('.dots').remove()
-        //         break;
-        //     }
-
-        // }
 
     }
 
-   
-
-    //create a loop that appends text select the last character of the string and append it using a span tag then use the ID of the span tag to check its position in the window.
-
-    //if it has not reached the outer most edge of the table coordinate remove the span tag and continue the loop
-
-    //if it has end the function
-
-    //https://stackoverflow.com/questions/5143534/how-to-get-the-position-of-text-within-an-element
-
-
+    //function loops through the table element finds all the p tags then runs dot function within the loop.
+    function addToString() {
+        let table = document.getElementsByTagName('table')
+        for (var i = 0; i < table.length; i++) {
+            let p = table[i].getElementsByTagName('p')[0]
+            console.log(p)
+            dots(p);
+        }
+    }
 
 });
