329409640af773097174bb0baf5adbe51a74322c
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-12-02 08:59:01 -0600

    finalizing while loop

--- public/js/menu.js
+++ public/js/menu.js
@@ -37,51 +37,25 @@ $(document).ready(function () {
         addToString();
     });
     // add error handling for ajax call and loading icon  because heroku is slow to load
-
-    // let testTable = document.getElementById('test');
-    // console.log(testTable)
-    // let leftPoint = testTable.getBoundingClientRect();
-    // console.log(leftPoint);
-
-    //prints dots to the p tags within the table element, will want to replace 100 with a length or do a while loop instead
-    //outer point will need to be replaced at aswell, last p tag dots run past because using from LEFT instead of from right elements write from left to right every other P tag should cross check against left point, even p tag should check against right point
-
-
-    function dots(x, y) {
-        for (var i = 0; i < 100; i++) {
-            //runs loop 100 times
+    function dots(x, y, a, b) {
+        while (a < b) {
             let dotSpan = '<span id = "dots">  .</span>'
-            $(x).append('  .' + dotSpan)
-            //appends a normal dot and a span dot for tracking
+            $(x).append(dotSpan)
             let getDot = document.getElementById('dots');
             let findMe = getDot.getBoundingClientRect();
-            // console.log(findMe)
-            //finds location of dot
             let fromLeft = findMe.left;
-            //location from left of dot
             dotPoint = Math.round(fromLeft)
-            // console.log(dotPoint)
-            //rounds the number
-            console.log('where I am' + dotPoint)
-            console.log('where i should stop:' + y)
-
             if (dotPoint > y) {
                 console.log('both together' + dotPoint, y)
                 $('#dots').remove();
                 break;
             } else {
-
+                $(x).append('  .')
                 $('#dots').remove();
-
             }
-
-        }//y vs dot point if dotpoint gets to be larger than y break the loop
-
+        }
     }
-
-
-
-    //function loops through the table element finds all the p tags then runs dot function within the loop.
+    //function loops through the table element finds all the p tags then runs dot function within the loop y passing its variables as arguments to the dots loop that only runs while we are cycling through the table elements(p tags in this case)
     function addToString() {
         let table = document.getElementsByTagName('table')
         for (var i = 0; i < table.length; i++) {
@@ -91,33 +65,11 @@ $(document).ready(function () {
             console.log(roundedRightPoint)
             let offSetNum = roundedRightPoint - 100;
             let p = table[i].getElementsByTagName('p')[0]
-            console.log(p)
-            dots(p, offSetNum);
+            a = i;
+            b = table.length
+            dots(p, offSetNum, a, b);
         }
     }
 
 });
 
-
- // function dots(x, y) {
-    //     while (dotPoint < y) {
-    //         if (dotPoint > y) {
-    //             $('#dots').remove()
-    //             break;
-
-    //         } else {
-    //             let dotSpan = '<span id = "dots">  .</span>'
-    //             $(x).append('  .' + dotSpan)
-    //             let getDot = document.getElementById('dots');
-    //             let findMe = getDot.getBoundingClientRect();
-    //             console.log(findMe)
-    //             let fromLeft = findMe.left;
-    //             dotPoint = Math.round(fromLeft)
-
-    //         }
-
-    //     }
-
-
-
-    // }
